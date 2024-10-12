928c5d119fa9c1022981c2ff0781fea79bc2f5a6
pypsada <3223175866@qq.com>
2024-10-12 16:50:26 +0800

    脚本上传

---
+++ Assets/Scripts/Achievement/Whole.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whole : MonoBehaviour
+{
+    public static List<Achievement> achievements = new List<Achievement>();
+}
